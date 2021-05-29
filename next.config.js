e391efde4b0c7fddc9daef12bd68acc672e14ab3
NguyenHung <ken247199@gmail.com>
2021-05-29 17:15:12 +0700

    update UI notify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "https://ht-store.vercel.app",
+        "BASE_URL": "https://ht-store.herokuapp.com",
         "MONGODB_URL": 'mongodb+srv://dbUser:dbUser@database-shop.gqq2f.mongodb.net/AoThun_DB?retryWrites=true&w=majority',
         "ACCESS_TOKEN_SECRET": 'eKM&>Y]navx8:K<@>VuWW/K)y?Qm5?5B{S9Bap"QC"je6Ua`Ex',
         "REFRESH_TOKEN_SECRET": '9."nDn95_?!Z3B.`!$_CdDEh#pSg7RSFvGe^&FyU9UM=>Y^:E7+!sf?:z6>_.dMDY4Sx/*5@7":u6CA)',
